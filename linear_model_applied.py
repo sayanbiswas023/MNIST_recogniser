2ba8ebee53e29bba5e630be68764e1ae189f21c9
sayanbiswas023 <77921275+sayanbiswas023@users.noreply.github.com>
2021-03-10 23:06:20 +0530

    Update linear_model_applied.py

--- linear_model_applied.py
+++ linear_model_applied.py
@@ -31,8 +31,17 @@ x_test=x_test.to_numpy()
 y_train=y_train.to_numpy()
 y_test=y_test.to_numpy()
 
+#making a copy of y_test for performing accuracy tests
+y_check=y_test
+
+##normalising the numpy arrays
+x_train = (x_train  - x_train .min()) / (x_train .max() - x_train .min())
+y_train = (y_train  - y_train .min()) / (y_train .max() - y_train .min())
+x_test = (x_test  - x_test .min()) / (x_test .max() - x_test .min())
+y_test = (y_test  - y_test .min()) / (y_test .max() - y_test .min())
+
 ##training data fitted
-regressor = linearregression(learning_rate=0.01, n_iters=1000)
+regressor = linearregression(learning_rate=0.05, n_iters=1000)
 regressor.fit(x_train, y_train)
 
 ##making predictions
